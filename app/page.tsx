590500b130e83f526fbdd7707bf73093b08c6293
gitonga <gitonga.deus@gmail.com>
2023-04-07 22:58:01 +0300

    minor fix and update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 
 import Intro from '@/components/intro'
+import CustomLink from '@/components/link';
 
-import { allBlogs } from 'contentlayer/generated';
+import { allArticles } from 'contentlayer/generated';
 
 import { compareDesc, format, parseISO } from 'date-fns';
 
@@ -11,24 +12,24 @@ const Home = () => {
 		<section>
 			<Intro />
 
-			<h1 className='text-xl mb-1 tracking-tight font-semibold leading-8'>Blog Posts</h1>
+			<h1 className='text-base tracking-tight font-semibold leading-8'>Recent Articles</h1>
 
-			{allBlogs.slice(0, 4).sort((a, b) => (compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))))
-				.map((post) => {
-					const { slug, title, summary, publishedAt, readingTime } = post;
+			{allArticles.slice(0, 4).sort((a, b) => (compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))))
+				.map((article) => {
+					const { slug, title, summary, publishedAt, readingTime } = article;
 
 					return <>
 						<Link
 							key={slug}
-							className='block select-none p-2 my-2 -mx-2 rounded-md transition-colors hover:bg-gray-100'
-							href={`/blog/${slug}`}
+							className='block select-none p-2 my-1 -mx-2 rounded-md duration-500 transition-colors hover:bg-gray-200'
+							href={`article/${slug}`}
 						>
 							<div className='flex py-1 items-center text-accent-5 gap-2 text-sm tracking-tight'>
 								<time dateTime={publishedAt}>
 									{format(parseISO(publishedAt), 'MMM dd, yyyy')}
 								</time>
-								<div className='w-[1px] h-[15px] bg-accent-5' />
-								<span className='capitalize'>{readingTime.text}</span>
+								&#8226;
+								<span>{readingTime.text}</span>
 							</div>
 
 							<h2 className='py-1 text-base font-semibold leading-6 tracking-tight'>{title}</h2>
@@ -38,13 +39,13 @@ const Home = () => {
 				})
 			}
 
-			<div className='flex '>
-				<Link
+			<div className='flex'>
+				<CustomLink
 					href='/blog'
-					className='my-2 hover:bg-gray-100 hover:text-black p-2 -mx-2 rounded-md flex items-center gap-4 text-[15px] font-medium text-accent-5'
+					className='my-2 bg-gray-200 text-black p-2 -mx-2 rounded-md flex items-center gap-4 text-[15px] font-medium'
 				>
-					<span>more posts...</span>
-				</Link>
+					more posts...
+				</CustomLink>
 			</div>
 		</section>
 	)
