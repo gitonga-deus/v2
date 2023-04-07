945cf456bd067a7eb991f92c7f1283caaed72f41
gitonga <gitonga.deus@gmail.com>
2023-04-07 23:18:22 +0300

    add fonts for the website

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Abril_Fatface, Josefin_Slab } from 'next/font/google'
+import { Inter, Abril_Fatface } from 'next/font/google'
 
 export const inter = Inter({
 	display: "swap",
@@ -12,11 +12,4 @@ export const abril = Abril_Fatface({
 	subsets: ["latin"],
 	variable: "--font-arvo",
 	weight: ["400"],
-});
-
-export const josefin = Josefin_Slab({
-	display: "swap",
-	subsets: ["latin"],
-	variable: "--font-josefin",
-	weight: ["400", "600", "700"],
 });
