cf99946d29e9628641e12a8c6fa279e7dbaf28d2
gitonga <gitonga.deus@gmail.com>
2023-04-10 16:23:22 +0300

    remove the spotfiy now-playing feature

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,16 @@
-import NowPlaying from './nowPlaying'
-
 const Footer = () => {
 	return (
 		<footer className="max-w-3xl my-8">
 			<hr className="my-4 -mx-2 text-shade-3" />
-			<div className="flex flex-col items-center justify-between mt-6 md:flex-row-reverse">
-				<div className="text-sm items-center py-1 font-medium text-black">
-					&copy; {new Date().getFullYear()}
-				</div>
 
-				<NowPlaying />
+			<div className="flex text-sm items-center py-1 font-medium text-black uppercase">
+				&copy; {new Date().getFullYear()}
+				<div className='w-[1px] h-[20px] mx-[5px] bg-shade-3' />
+				Deus Gitonga.
 			</div>
+
+
+
 		</footer>
 	)
 }
