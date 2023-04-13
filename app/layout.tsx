a2978186c05bd87811c9b6ea5c82603e96087a33
gitonga <gitonga.deus@gmail.com>
2023-04-14 02:31:47 +0300

    feat: add a bg gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react'
 import KBar from '@/components/kbar'
 import { inter } from '@/utils/fonts'
 import { Navbar, Footer } from '@/components/layout'
+import { clsx } from 'clsx'
 
 export const metadata: Metadata = {
 	title: {
@@ -32,6 +33,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		>
 			<KBar>
 				<body className='antialiased select-none overflow-x-hidden max-w-3xl relative mx-auto pt-20 px-8'>
+					<span
+						className={clsx(
+							"rounded-full bg-gradient-to-r from-blue-300 via-cyan-200 to-green-300 opacity-70",
+							"-z-50 aspect-square w-full max-w-screen-lg blur-3xl filter",
+							"bottom-[calc(100%-200px)]",
+							"absolute"
+						)}
+					/>
 					<Navbar />
 					{children}
 					<Footer />
