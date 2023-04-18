5ebb9147f186081da25446defadd1bd66a818428
gitonga <gitonga.deus@gmail.com>
2023-04-18 14:57:38 +0300

    minor update

--- lib/rehypePrettyCode.ts
+++ lib/rehypePrettyCode.ts
@@ -1,7 +1,21 @@
-import { type Options } from 'rehype-pretty-code'
+import { Options } from 'rehype-pretty-code'
 
 export const rehypePrettyCodeOptions: Partial<Options> = {
-	// Use a prepackaged theme
-	theme: 'one-dark-pro'
+	theme: {
+		light: 'github-light',
+		dark: 'github-dark',
+	},
+	onVisitLine(node: any) {
+		// Prevent lines from collapsing in `display: grid` mode, and
+		// allow empty lines to be copy/pasted
+		if (node.children.length === 0) {
+			node.children = [{ type: 'text', value: ' ' }]
+		}
+	},
+	onVisitHighlightedLine(node: any) {
+		node.properties.className.push('highlighted')
+	},
+	onVisitHighlightedWord(node: any) {
+		node.properties.className = ['word']
+	},
 }
-
