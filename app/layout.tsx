0f80e023a783b299df49f272210e06024c7bb880
gitonga <gitonga.deus@gmail.com>
2023-04-19 01:59:16 +0300

    add site config

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,21 +10,57 @@ import CustomToaster from './custom-toaster'
 import { roboto, inter } from '@/utils/fonts'
 import { Navbar, Footer } from '@/components/layout'
 
+import { site } from '@/config/site'
+
 export const metadata: Metadata = {
 	title: {
-		default: 'Deus Gitonga',
-		template: '%s | Deus Gitonga',
+		default: site.title,
+		template: `%s ${site.titleTemplate}`,
 	},
-	description: 'Developer & Writer.',
+	description: site.description,
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	// manifest: '/static/favicon/site.webmanifest',
+	keywords: site.keywords,
+	themeColor: '#0d1117',
 	openGraph: {
-		title: 'Deus Gitonga',
-		description: 'Developer & Writer.',
-		url: 'https://gitonga.me',
-		siteName: 'Deus Gitonga',
-		images: [],
-		locale: 'en_US',
+		url: `${site.url}`,
 		type: 'website',
-	}
+		title: site.title,
+		siteName: site.title,
+		description: site.description,
+		locale: 'en-US',
+		// images: [
+		// 	{
+		// 		url: `${site.url}/static/images/og/og.png`,
+		// 		width: 1200,
+		// 		height: 630,
+		// 		alt: site.description,
+		// 		type: 'image/png',
+		// 	},
+		// ],
+	},
+	icons: {
+		icon: '../public/favicon.ico',
+		shortcut: '../public/favicon.ico',
+		apple: [
+			{
+				url: '../public/apple-touch-icon.png',
+				sizes: '180x180',
+				type: 'image/png',
+			},
+		],
+		other: [...site.favicons],
+	},
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- config/site.ts
+++ config/site.ts
@@ -16,25 +16,24 @@ type Site = {
 
 export const site: Site = {
 	url: isProduction ? 'https://gitonga.me' : 'http://localhost:3000',
-	// logo: '',
 	title: 'Deus Gitonga',
 	name: 'Deus Gitonga',
 	keywords: ['Deus Gitonga', 'Next.js', 'React', 'TypeScript', 'Frontend Developer'],
 	titleTemplate: '| Deus Gitonga',
-	description: 'Developer',
+	description: 'Developer & Writer',
 	githubUsername: 'gitonga-deus',
 	favicons: [
 		{
 			rel: 'icon',
 			type: 'image/png',
 			sizes: '16x16',
-			url: '/static/favicon/favicon-16x16.png',
+			url: '../public/favicon-16x16.png',
 		},
 		{
 			rel: 'icon',
 			type: 'image/png',
 			sizes: '32x32',
-			url: '/static/favicon/favicon-32x32.png',
+			url: '../public/favicon-32x32.png',
 		},
 	],
 }
