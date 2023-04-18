e7ba4046595cfb992edbd7dcdbc6e2565b42da37
gitonga <gitonga.deus@gmail.com>
2023-04-18 18:05:35 +0300

    add Link component

---
+++ components/mdx/link.tsx
@@ -0,0 +1,36 @@
+import NextLink from 'next/link'
+import React from 'react'
+
+const Link = (props: any) => {
+	const { href, children, ...rest } = props
+
+	if (href.startsWith('/')) {
+		return (
+			<NextLink href={href} className='animated' {...rest}>
+				{children}
+			</NextLink>
+		)
+	}
+
+	if (href.startsWith('#')) {
+		return (
+			<a href={href} {...rest}>
+				{children}
+			</a>
+		)
+	}
+
+	return (
+		<a
+			target='_blank'
+			rel='noopener noreferrer'
+			href={href}
+			className='animated'
+			{...rest}
+		>
+			{children}
+		</a>
+	)
+}
+
+export default Link
