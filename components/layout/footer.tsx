5822410a006a7bed6dc4aa58da7d6c0c61730c68
gitonga <gitonga.deus@gmail.com>
2023-04-20 22:31:30 +0300

    fix

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,11 +3,10 @@ const Footer = () => {
 		<footer className="max-w-3xl my-8">
 			<hr className="my-4 -mx-2 text-shade-3" />
 
-			<div className="flex text-sm items-center gap-1 py-1 font-medium text-black">
-				&copy;{new Date().getFullYear()}
-				<h1 className="">Deus Gitonga</h1>
-				<hr className="w-[16px] text-black" />
-				<i className="">Nairobi, Kenya</i>
+			<div className="flex text-sm items-center py-1 font-medium text-black uppercase">
+				&copy; {new Date().getFullYear()}
+				<div className='w-[1px] h-[20px] mx-[5px] bg-shade-3' />
+				Deus Gitonga.
 			</div>
 
 
