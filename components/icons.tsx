4268fa42349fb6822e251916ef8b91fae9ba79aa
gitonga <gitonga.deus@gmail.com>
2023-04-24 00:42:38 +0300

    add ChevronLeft icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -151,3 +151,14 @@ export const Check = () => {
 		</svg>
 	)
 }
+
+export const ChevronLeft = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-shade-3" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+			<polyline points="15 6 9 12 15 18" />
+		</svg>
+	)
+}
