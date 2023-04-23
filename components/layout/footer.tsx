16347e199bd3ab31aa02f1b20943c515deb4736f
gitonga <gitonga.deus@gmail.com>
2023-04-24 00:45:05 +0300

    add border-top to match the border-bottom for the navbar

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,12 +1,13 @@
 const Footer = () => {
 	return (
-		<footer className="max-w-3xl mx-auto my-8 px-8">
-			<hr className="my-4 -mx-2 text-shade-3" />
+		<footer>
+			<div className="py-4 max-w-3xl border-t mx-auto px-8">
 
-			<div className="flex text-sm items-center py-1 font-medium text-black uppercase">
-				&copy; {new Date().getFullYear()}
-				<div className='w-[1px] h-[20px] mx-[5px] bg-shade-3' />
-				Deus Gitonga.
+				<div className="flex text-sm items-center py-1 font-medium text-black uppercase">
+					&copy; {new Date().getFullYear()}
+					<div className='w-[1px] h-[15px] mx-2 bg-shade-3' />
+					Deus Gitonga.
+				</div>
 			</div>
 		</footer>
 	)
