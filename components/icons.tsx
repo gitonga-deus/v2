08ec2b394751077dde7be74583e2567b4d0cffd9
gitonga <gitonga.deus@gmail.com>
2023-04-26 02:24:47 +0300

    feat: update command's icon size

--- components/icons.tsx
+++ components/icons.tsx
@@ -103,7 +103,7 @@ export function Link() {
 export function Command() {
 	return (
 		<svg
-			xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 font-bold"
+			xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"
 			viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
 			fill="none" strokeLinecap="round" strokeLinejoin="round"
 		>
