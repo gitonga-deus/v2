149ce01ea04f7bb0802eb77549fe4bc8c8f9e124
gitonga <gitonga.deus@gmail.com>
2023-04-26 00:11:03 +0300

    feat: update font-size

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 		<footer>
 			<div className="py-4 max-w-3xl border-t mx-auto px-8">
 
-				<div className="flex text-base items-center py-1 font-medium text-black uppercase">
+				<div className="flex text-sm items-center py-1 font-medium text-black uppercase">
 					&copy; {new Date().getFullYear()}
 					<div className='w-[1px] h-[15px] mx-1 bg-shade-3' />
 					Deus Gitonga.
