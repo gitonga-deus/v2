21a027b52025a90a87e0365da2b386ba4c383052
gitonga <gitonga.deus@gmail.com>
2023-04-28 01:10:51 +0300

    update

--- app/articles/[slug]/header.tsx
+++ app/articles/[slug]/header.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { parseISO, format } from 'date-fns'
 
-import Link from '@/components/mdx/link'
+import Link from 'next/link'
 import { ChevronLeft } from '@/components/icons'
 
 
@@ -17,25 +17,18 @@ const Header = (props: HeaderProps) => {
 	const { date, title, readingTime } = props
 
 	return (
-		<div className="pt-6">
-			<div className='my-8 -mx-2 flex items-center font-medium text-sm'>
-				<ChevronLeft />
-				<Link href='/articles' className='text-shade-2'>
-					Back to Articles
-				</Link>
-			</div>
-
-			<h1 className='text-shade-1 text-xl md:text-3xl font-semibold'>
+		<div className="pt-12">
+			<h1 className='text-shade-1 text-xl md:text-2xl font-semibold'>
 				{title}
 			</h1>
 
-			<div className='flex py-2 items-center text-shade-2 text-sm gap-1 tracking-tight'>
+			<code className='flex py-2 items-center text-shade-3 text-sm gap-1 tracking-tight'>
 				<time dateTime={date}>
 					{format(parseISO(date), 'MMM dd, yyyy')}
 				</time>
 				/
 				<span>{readingTime}</span>
-			</div>
+			</code>
 		</div>
 	)
 }
