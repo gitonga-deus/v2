f81185919a32ff13860e1c05c56a0decfbfc1026
gitonga <gitonga.deus@gmail.com>
2023-05-03 14:37:33 +0300

    📦 NEW: add lastUpdate feature

--- app/articles/[slug]/content.tsx
+++ app/articles/[slug]/content.tsx
@@ -1,6 +1,9 @@
 import { Article } from 'contentlayer/generated'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 
+import { isSameDay, parseISO, format } from 'date-fns'
+import { HiOutlinePencil } from 'react-icons/hi'
+
 import MDXComponents from '@/components/mdx/mdxcomponents'
 
 type ContentProps = {
@@ -11,11 +14,24 @@ type ContentProps = {
 const Content = (props: ContentProps) => {
 	const { article, slug } = props
 
+	const hasBeenUpdated = !isSameDay(
+		new Date(article.publishedAt),
+		new Date(article.lastUpdatedDate)
+	)
+
 	const MDXComponent = useMDXComponent(article.body.code)
 
 	return (
 		<div className='mt-4 flex flex-col justify-between lg:flex-row'>
 			<article className='prose text-base text-left text-shade-2 tracking-tight leading-8 max-w-none w-full'>
+				{hasBeenUpdated && (
+
+					<div className='py-2 px-3 bg-color-1 gap-1 flex w-52 items-center text-sm rounded-md text-shade-1'>
+						<HiOutlinePencil size={20} />
+						Updated {format(parseISO(article.lastUpdatedDate), 'MMM dd, yyyy')}
+					</div>
+
+				)}
 				<MDXComponent
 					components={{
 						...MDXComponents,
