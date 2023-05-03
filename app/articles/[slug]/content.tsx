8ae25c85e9c175c94aed38a859bc00cb840e90f7
gitonga <gitonga.deus@gmail.com>
2023-05-03 19:52:25 +0300

    update text-color

--- app/articles/[slug]/content.tsx
+++ app/articles/[slug]/content.tsx
@@ -23,7 +23,7 @@ const Content = (props: ContentProps) => {
 
 	return (
 		<div className='mt-4 flex flex-col justify-between lg:flex-row'>
-			<article className='prose text-base text-left text-shade-3 tracking-tight leading-8 max-w-none w-full'>
+			<article className='prose text-base text-left text-shade-2 tracking-tight leading-8 max-w-none w-full'>
 				{hasBeenUpdated && (
 
 					<div className='py-2 px-3 bg-color-1 gap-1 flex w-52 items-center text-sm rounded-md text-shade-1'>
