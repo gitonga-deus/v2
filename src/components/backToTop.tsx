4294a592e238f3a8b644692eae52c9554f2fecc2
gitonga <gitonga.deus@gmail.com>
2023-05-09 16:38:31 +0300

    feat: update backToTop icon(from react-icons to tabler-icons)

--- src/components/backToTop.tsx
+++ src/components/backToTop.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { FiChevronUp } from 'react-icons/fi'
+import { IconChevronUp } from '@tabler/icons-react'
 
 export default function BackToTop() {
 	const [showButton, setShowButton] = useState(false)
@@ -24,7 +24,7 @@ export default function BackToTop() {
 			style={{ transform: showButton ? 'rotate(0deg)' : 'rotate(180deg)' }}
 			aria-label='Back to top'
 		>
-			<FiChevronUp size={24} />
+			<IconChevronUp size={24} />
 		</button>
 	)
 }
