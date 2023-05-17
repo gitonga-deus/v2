664727fdc9760ed9f25943d4eb4b745849b3d37b
gitonga <gitonga.deus@gmail.com>
2023-05-17 15:56:18 +0300

    minor style update

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
 				"art-hv": "var(--art-hv)"
 			},
 			fontFamily: {
-				default: ['var(--font-karla)'],
+				// default: ['var(--font-karla)'],
 				code: [
 					'var(--font-operator)',
 				],
