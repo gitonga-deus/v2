06ce7250d4e3373a46ceb0cb4388de714b6b09cd
gitonga <gitonga.deus@gmail.com>
2023-06-14 18:33:06 +0300

    feat: add error page

---
+++ app/error.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	useEffect(() => {
+		
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div>
+			<p>Oops, something went wrong... maybe refresh?</p>
+		</div>
+	);
+}
