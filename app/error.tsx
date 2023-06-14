13a09e4dffab7a7f079a76bffd2d2bb99a13344e
gitonga <gitonga.deus@gmail.com>
2023-06-14 18:59:48 +0300

    minor fix

--- app/error.tsx
+++ app/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
 	}, [error]);
 
 	return (
-		<div className="bg-white py-6 sm:py-8 lg:py-12">
+		<div className="py-6 sm:py-8 lg:py-12">
 			<div className="mx-auto px-4 md:px-8">
 				<div className="flex flex-col items-center">
 					<p
@@ -31,14 +31,14 @@ export default function Error({
 					</h1>
 
 					<p
-						className="mb-12 max-w-screen-md text-center text-gray-500 md:text-lg"
+						className="mb-12 text-center text-gray-500 text-base"
 					>
 						The page you’re looking for doesn’t exist.
 					</p>
 
 					<Link
 						href="/"
-						className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
+						className="inline-block rounded-lg bg-gray-100 px-2 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-200 focus-visible:ring active:text-gray-700 md:text-base"
 					>
 						Go home
 					</Link>
