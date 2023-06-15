0457210a7f30fb40f8efc151b3bb096f35bc6094
gitonga <gitonga.deus@gmail.com>
2023-06-15 14:51:35 +0300

    feat: update font size

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
 		<footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8 mt-16">
 			<hr className="w-full border-1 border-gray-200 mb-8" />
 
-			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 text-base sm:grid-cols-3">
 				<div className="flex flex-col space-y-4">
 					<Link href="/">
 						<p className="text-gray-500 hover:text-gray-600 transition">Home</p>
