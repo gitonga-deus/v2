0101d03b74a9850ca123d20ce6fda7b6d6a0df1c
gitonga <gitonga.deus@gmail.com>
2023-06-16 04:17:51 +0300

    add more icons

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -45,3 +45,56 @@ export function Search() {
 		</>
 	)
 }
+
+export function Calendar() {
+	return (
+		<>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none" viewBox="0 0 24 24"
+				strokeWidth={1.5} stroke="currentColor"
+				className="w-5 h-5"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+				/>
+			</svg>
+		</>
+	)
+}
+
+export function Clock() {
+	return (
+		<>
+			<svg
+				xmlns="http://www.w3.org/2000/svg" fill="none"
+				viewBox="0 0 24 24" strokeWidth={1.5}
+				stroke="currentColor" className="w-5 h-5"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+				/>
+			</svg>
+
+		</>
+	)
+}
+
+export function ArrowRight() {
+	return (
+		<>
+			<svg
+				className="w-5 h-5" viewBox="0 0 15 15"
+				fill="none" xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M2.14645 11.1464C1.95118 11.3417 1.95118 11.6583 2.14645 11.8536C2.34171 12.0488 2.65829 12.0488 2.85355 11.8536L6.85355 7.85355C7.04882 7.65829 7.04882 7.34171 6.85355 7.14645L2.85355 3.14645C2.65829 2.95118 2.34171 2.95118 2.14645 3.14645C1.95118 3.34171 1.95118 3.65829 2.14645 3.85355L5.79289 7.5L2.14645 11.1464ZM8.14645 11.1464C7.95118 11.3417 7.95118 11.6583 8.14645 11.8536C8.34171 12.0488 8.65829 12.0488 8.85355 11.8536L12.8536 7.85355C13.0488 7.65829 13.0488 7.34171 12.8536 7.14645L8.85355 3.14645C8.65829 2.95118 8.34171 2.95118 8.14645 3.14645C7.95118 3.34171 7.95118 3.65829 8.14645 3.85355L11.7929 7.5L8.14645 11.1464Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"
+				/>
+			</svg>
+		</>
+	)
+}
