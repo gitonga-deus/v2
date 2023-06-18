d66f297f401263e11940afb4995603bb2880f392
gitonga <gitonga.deus@gmail.com>
2023-06-18 16:46:46 +0300

    minor update

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 
+import { SOCIAL_LINKS } from '@/config';
+
 const ExternalLink = ({ href, children }: any) => (
 	<a
-		className="text-gray-500 hover:text-gray-600 transition"
+		className="text-gray-500 hover:text-gray-800 transition hover:font-semibold"
 		target="_blank"
 		rel="noopener noreferrer"
 		href={href}
@@ -16,29 +18,39 @@ export default function Footer() {
 		<footer className="flex flex-col justify-center text-base items-start max-w-2xl mx-auto w-full mb-8 mt-16">
 			<hr className="w-full border-1 border-gray-200 mb-8" />
 
-			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-12 sm:grid-cols-3">
 				<div className="flex flex-col space-y-4">
 					<Link href="/">
-						<p className="text-gray-500 hover:text-gray-600 transition">Home</p>
+						<p className="text-gray-500 hover:text-gray-800 hover:font-semibold transition">
+							Home
+						</p>
 					</Link>
 					<Link href="/about">
-						<p className="text-gray-500 hover:text-gray-600 transition">
+						<p className="text-gray-500 hover:text-gray-800 hover:font-semibold transition">
 							About
 						</p>
 					</Link>
 					<Link href="/articles">
-						<p className="text-gray-500 hover:text-gray-600 transition">
+						<p className="text-gray-500 hover:text-gray-800 hover:font-semibold transition">
 							Articles
 						</p>
 					</Link>
 				</div>
 				<div className="flex flex-col space-y-4">
-					<ExternalLink href="https://github.com/gitonga-deus">GitHub</ExternalLink>
-					<ExternalLink href="https://www.linkedin.com/in/gitonga-deus/">LinkedIn</ExternalLink>
+					<ExternalLink href={'https://gitonga.me/rss.xml'}>RSS</ExternalLink>
+
+					{SOCIAL_LINKS.map((social, i) => (
+						<ExternalLink
+							key={i}
+							href={social.href}
+						>
+							{social.text}
+						</ExternalLink>
+					))}
 				</div>
 				<div className="flex flex-col space-y-4">
 					<Link href="/snippets">
-						<p className="text-gray-500 hover:text-gray-600 transition">
+						<p className="text-gray-500 hover:text-gray-800 hover:font-semibold transition">
 							Snippets
 						</p>
 					</Link>
